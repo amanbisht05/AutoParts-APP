ab0764db427ed7880ab2773ff6071dec2901a6b1
Aman Bisht <aamanbisht99@gmail.com>
2025-03-17 15:14:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+import threading
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+@app.get("/")
+def serve_ui():
+    return FileResponse("static/index.html")
+
+
+
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
